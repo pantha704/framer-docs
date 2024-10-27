e4c673462117322285df51918ced29ada8414a50
pantha704 <prathamjaiswal204@gmail.com>
2024-10-27 20:37:30 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import {
-  motion,
-  AnimatePresence,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
 import "./index.css";
 import { Suspense, lazy } from "react";
 
-const Home = lazy(() => import("./pages/home"));
-const Docs = lazy(() => import("./pages/docs"));
+const Home = lazy(() => import("./pages/Home"));
+const Docs = lazy(() => import("./pages/Docs"));
 const Egghead = lazy(() => import("./pages/Egghead"));
-
-import { Dialog } from "daisyui"; // Import Daisy UI Dialog for better modal
-import { FaRocket, FaHandPeace, FaCogs } from "react-icons/fa"; // Importing icons from react-icons
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
